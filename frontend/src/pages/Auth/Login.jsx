2892bdbe2887dd1c82118a54d2d6050def0ad96b
Ravi Chauhan <ravischauhan06@gmail.com>
2025-10-09 23:54:53 +0530

    login page cooompleted

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
-import{Link,useLocation, useNavigate} from 'react-router-dom';
+import{useLocation, useNavigate} from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { useLoginMutation } from "../../redux/api/usersApiSlice";
-import { setCredentials } from "../../redux/slices/authSlice";
-import { toast } from "react-toastify";
-
+import Loader from "../../components/Loader";
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
@@ -39,15 +38,63 @@ const Login = () => {
 
       <section className="pl-[10rem] flex flex-wrap">
       <div className="mr-[ 4rem ] mt-[5rem]"> 
-        <h1 className="text-2xl font-semibold mb-4">Login</h1>
+        <h1 className="text-2xl font-semibold mb-4">Sign IN</h1>
+
+
+        <form className= "container w-[40rem]">
+        <div className ="my-[2rem]">
+          <label
+       htmlFor="email" 
+       className="block text-sm font-medium text-black "
+       >
+        Email Address
+        </label>  
+
+      <input 
+      type="email"
+      id="email"
+      value={email}
+      onChange={(e) => setEmail(e.target.value)}
+      placeholder="Enter email"
+      className="mt-1 p-2 border rounded w-full"
+      /> 
+           </div>
+            <div className ="my-[2rem]">
+          <label
+       htmlFor="Password" 
+       className="block text-sm font-medium text-black "
+       >
+        Password
+        </label>  
+
+      <input 
+      type="Password"
+      id="Password"
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      placeholder="Enter Password"
+      className="mt-1 p-2 border rounded w-full"
+      /> 
+           </div>
+
+           <button disabled={isLoading}
+                    type="submit"
+                   className="bg-blue-500 text-white px-4 py-2 rounded-cursor-pointer my-[1rem] ">{isLoading ? "Signing In..." : "Sign In"}
+           </button>   
+
+           {isLoading && <Loader />}
+           </form> 
+           <div className="mt-4">
+            <p className="text-whte">
+            New Customer?{' '}
+            <Link to ={redirect ? `/register?redirect=${redirect}` : '/register'} 
+            className="text-blue-500 hover:underline">Regster</Link>
+            </p>  
+            </div>
         </div> 
       </section>
-
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
